dca73eb2a05abf771b0517c65cae9bbc6a10a369
Amr maher <amrr.maherr24@gmail.com>
2024-09-24 12:32:49 +0300

    feat: Add functionality to load data and update badge count on window load  - Implemented GetData() function call on window load to fetch recipes. - Updated ShowData function to render recipes dynamically. - Added upDateBadge function to update the badge count based on the number of recipes in the cart.
    feat: Add functionality to load data and update badge count on window load
    
    - Implemented GetData() function call on window load to fetch and display recipes.
    - Updated ShowData function to render recipes dynamically using a template string.
    - Added upDateBadge function to update the badge count based on the number of recipes in the cart.
    - The badge now reflects the total count of recipes currently added to the cart, enhancing user experience.

--- js/main.js
+++ js/main.js
@@ -36,5 +36,38 @@ function GetData() {
   xml.send();
 }
 
+window.onload = function () {
+  GetData();
+  upDateBadge();
+};
+
+let recipesRow = document.querySelector(".recipesRow");
+let AllRecipes = []; // all recipes array
+function ShowData(recipes) {
+  AllRecipes = recipes;
+  console.log(AllRecipes);
+  let recipesHtml = AllRecipes.map(function (recipe) {
+    return `<div class='col-md-4 col-12 my-4'>
+                <div class="card mx-auto" style="width: 18rem;">
+          <img src="${recipe.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${recipe.name}</h5>
+            <p class="card-text">tags: ${recipe.tags}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>`;
+  }).join("");
+  recipesRow.innerHTML = recipesHtml;
+}
+
+
+function upDateBadge() {
+  let badge = document.querySelector(".badge");
+  let recipesCartLength = document.querySelectorAll(".recipesCart .col-12");
+  badge.innerHTML = recipesCartLength.length;
+}
+
+
 
 
