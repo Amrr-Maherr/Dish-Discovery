c788f6ab870facfcb2185e9492923ac0d43e3afa
Amr maher <amrr.maherr24@gmail.com>
2024-09-26 11:37:10 +0300

    Title: Display recipes from localStorage and implement delete functionality  - Added `loadDataInPage` function to dynamically load and display recipes stored in `localStorage`. - Displayed recipe details such as image, name, meal type, preparation time, servings, rating, ingredients, and instructions. - Included a delete button for each recipe with a `DeleteRecipe` function to remove the recipe from the page. - Added confirmation prompt before deleting the recipe to prevent accidental removal. - Improved user interaction by providing a seamless experience for viewing and managing saved recipes.

--- js/favRec.js
+++ js/favRec.js
@@ -23,3 +23,53 @@ arrow.onclick = function () {
   localStorage.clear();
   window.location = "login.html";
 };
+let NewRecipe = document.querySelector(".NewRecipe");
+function loadDataInPage() {
+  let LocalData = JSON.parse(localStorage.getItem("Recipes"));
+  LocalData.forEach((recipe) => {
+    let cartHtml = `<div class='col-md-6 col-12 my-4'>
+          <div class="card mb-3 mx-auto" id='recipe${recipe.id}'>
+  <img src="${recipe.image}" class="card-img-top" alt="...">
+ <div class="card-body">
+  <h5 class="card-title">${recipe.name}</h5>
+  
+  <p class="card-text"><strong>Meal Type: </strong>${recipe.mealType}</p>
+  <p class="card-text"><strong>Preparation Time: </strong>${
+    recipe.prepTimeMinutes
+  } minutes</p>
+  <p class="card-text"><strong>Servings: </strong>${recipe.servings}</p>
+  <p class="card-text"><strong>Rating: </strong>${recipe.rating}</p>
+  
+  <hr>
+  
+  <p class="card-text"><strong>Ingredients: </strong>${recipe.ingredients.join(
+    ", "
+  )}</p>
+  
+  <hr>
+  
+  <p class="card-text"><strong>Instructions: </strong>${recipe.instructions}</p>
+  <hr>
+  <button onclick=DeleteRecipe(${recipe.id}) class='btn btn-danger'>Delete Recipe</button>
+</div>
+</div>
+    </div>`;
+    NewRecipe.innerHTML += cartHtml;
+  });
+}
+
+
+function DeleteRecipe(id) {
+    let warning = confirm(
+      "Are you sure you want to delete this recipe? This action cannot be undone."
+    );
+    if (warning) {
+      let deletedRecipe = document.querySelector(`#recipe${id}`);
+      deletedRecipe.remove();
+      let LocalData = JSON.stringify(localStorage.getItem("Recipes")) || []
+    }
+}
+
+window.onload = function () {
+  loadDataInPage();
+}
