bd0f6ee8aac03696ce769872b80cd4d81b60082f
Amr maher <amrm80233@>
2024-09-26 11:50:04 +0300

    Title: Update multiple files for improved functionality and design
    - Modified CSS styles in `css/style.css` for enhanced layout and typography.
    - Updated `favRec.html`, `index.html`, `login.html`, `recipes.html`, and `signin.html` for better UI and user experience.
    - Added new JavaScript functionality in `js/favRecipe.js` for managing favorite recipes.
    - Improved the main script in `js/main.js` for better performance and organization.

--- js/main.js
+++ js/main.js
@@ -30,11 +30,95 @@ function GetData() {
     if (this.readyState == 4 && this.status == 200) {
       let recipes = JSON.parse(this.responseText);
       ShowData(recipes.recipes);
+      console.log(recipes);
     }
   };
   xml.open("GET", "https://dummyjson.com/recipes", true);
   xml.send();
 }
 
+let recipesRow = document.querySelector(".recipesRow");
+let AllRecipes = []; // all recipes array
+function ShowData(recipes) {
+  AllRecipes = recipes;
+  let recipesHtml = AllRecipes.map(function (recipe) {
+    return `<div class='col-md-4 col-12 my-4'>
+    <div class="card mx-auto" style="width: 18rem;">
+        <img src="${recipe.image}" class="card-img-top" alt="${recipe.name}">
+        <div class="card-body">
+            <h5 class="card-title">${recipe.name}</h5>
+            <p class="card-text"><strong>Tags:</strong> ${recipe.tags}</p>
+            <p class="card-text"><strong>Prep Time:</strong> ${recipe.prepTimeMinutes} minutes</p>
+            <p class="card-text"><strong>Servings:</strong> ${recipe.servings}</p>
+            <a href="#" onclick="addToCart(event, ${recipe.id})" class="btn btn-primary">View Recipe Details</a>
+        </div>
+    </div>
+</div>
+`}).join("");
+  recipesRow.innerHTML = recipesHtml;
+}
 
+function addToCart(event, id) {
+  event.preventDefault();
+  let localStoRecipe = JSON.parse(localStorage.getItem("Recipes")) || [];
+  let chosenRecipe = AllRecipes.find((recipe) => recipe.id == id);
+  let cartHtml = `<div class='col-md-4 col-12 my-4'>
+                <div class="card mx-auto" style="width: 18rem;">
+          <img src="${chosenRecipe.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${chosenRecipe.name}</h5>
+            <p class="card-text">tags: ${chosenRecipe.tags}</p>
+          </div>
+        </div>
+      </div>`;
+  recipesCart.innerHTML += cartHtml;
+  localStoRecipe.push(chosenRecipe);
+  localStorage.setItem("Recipes", JSON.stringify(localStoRecipe));
+  upDateBadge();
+}
 
+let recipesCart = document.querySelector(".recipesCart");
+function loadDataInCart() {
+  let localStoRecipe = JSON.parse(localStorage.getItem("Recipes")) || [];
+  localStoRecipe.forEach((recipe) => {
+    let cartHtml = `<div class='col-md-4 col-12 my-4'>
+          <div class="card mx-auto" style="width: 18rem;">
+              <img src="${recipe.image}" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">${recipe.name}</h5>
+                  <p class="card-text">tags: ${recipe.tags}</p>
+              </div>
+          </div>
+      </div>`;
+    recipesCart.innerHTML += cartHtml;
+  });
+  upDateBadge()
+}
+
+let deleteAllBtn = document.querySelector(".deleteAllBtn");
+function upDateBadge() {
+  let badge = document.querySelector(".badge");
+  let recipesCartLength = document.querySelectorAll(".recipesCart .col-12");
+  badge.innerHTML = recipesCartLength.length;
+  if (recipesCartLength.length >= 0) {
+    deleteAllBtn.classList.remove('d-none')
+  } else {
+    deleteAllBtn.classList.remove("d-block");
+  }
+}
+
+deleteAllBtn.onclick = function () {
+  DeleteAll()
+}
+
+function DeleteAll() {
+  recipesCart.innerHTML = ''
+  localStorage.removeItem('Recipes');
+  upDateBadge()
+}
+
+window.onload = function () {
+  GetData();
+  upDateBadge();
+  loadDataInCart();
+};
