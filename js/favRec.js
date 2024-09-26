1cec9a74ab64029ee8256b5cae8050c97bdd95a0
Amr maher <amrr.maherr24@gmail.com>
2024-09-26 11:35:35 +0300

    Title: Implement user login check and redirect logic  - Added logic to check the user's login status using `localStorage.getItem("UserName")`. - If the user is logged in, hide the login/signup buttons and display the user information. - If the user is not logged in, alert the user and redirect to the login page. - Used class manipulation (`add`/`remove`) to toggle visibility of UI elements based on login status. - Enhanced user experience by providing immediate feedback for unauthorized access to the recipes section.

--- js/favRec.js
+++ js/favRec.js
@@ -1,1 +1,19 @@
 
+let userName = localStorage.getItem("UserName");
+let buttons = document.querySelector(".buttons");
+let userInfo = document.querySelector(".userInfo");
+let userN = document.querySelector(".userN");
+
+function redirectBasedOnLoginStatus() {
+    if (userName) {
+    buttons.classList.add("d-none");
+    userInfo.classList.remove("d-none");
+    userN.innerHTML = userName;
+    } else {
+    alert("please login to see the Recipes");
+    userInfo.classList.add("d-none");
+    buttons.classList.remove("d-none");
+    window.location = "login.html";
+    }
+}
+redirectBasedOnLoginStatus();
